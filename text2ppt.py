cdf0fb28daba628b36bfd8c99daaaf6d94a41925
AbhijeetShankar <abhijeetshankar98@gmail.com>
2023-05-07 02:06:31 +0530

    PreFinal

--- text2ppt.py
+++ text2ppt.py
@@ -2,6 +2,7 @@ from pptx import Presentation
 from pptx.util import Inches,Pt
 from pptx.enum.text import MSO_ANCHOR, MSO_AUTO_SIZE
 #Create a new PowerPoint presentation
+
 prs = Presentation()
 
 def add_slide(prs, layout, title, subtitle):
@@ -21,19 +22,15 @@ def add_slide(prs, layout, title, subtitle):
     return slide
 
 
-
-capital_city = {"Nepal": "Kathmandu", "Italy": "Rome", "England": "London"}
-m=list(capital_city.keys())
-n=list(capital_city.values())
 #print(m[1])
 
-
+dic=[{"Topic":"Lawda","Summary":["1","2","3"]},{"Topic":"Muth","Summary":["3","2","1"]}]
 #Choose the layout as desired
 
 title_slide_layout = prs.slide_layouts[1]
 
-for i in range (0,len(m)):     
-    slide = add_slide(prs, title_slide_layout, m[i],n[i] )
+for i in range (0,len(dick)):     
+    slide = add_slide(prs, title_slide_layout, dic[i]["Topic"],"\n".join(dic[i]["Summary"]))
 
 
 # Save the presentation
