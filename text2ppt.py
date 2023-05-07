a9ce0c9f559993705f4b85574db8dc938dc469d6
AbhijeetShankar <abhijeetshankar98@gmail.com>
2023-05-07 22:29:52 +0530

    AddedImagetoPpt

--- text2ppt.py
+++ text2ppt.py
@@ -9,20 +9,13 @@ def presentate(defined_list):
     def add_slide(prs, layout, title, subtitle):
         slide = prs.slides.add_slide(layout)
         slide.shapes.title.text = title
-        textbox = slide.shapes.add_textbox(Inches(1), Inches(2), Inches(6), Inches(2))
-        # Add text to the text box
-        tf = textbox.text_frame
-        tf.text = subtitle
-        textbox.left = Inches(1)
-        textbox.top = Inches(2)
-        textbox.width = Inches(10)
-        textbox.height = Inches(2)
+        slide.placeholders[1].text=subtitle
         font = slide.shapes.title.text_frame.paragraphs[0].font
         font.name = 'Arial'
         font.size = Pt(30)
         font.bold = True
         font.italic = False
-        for x in tf.paragraphs:
+        for x in  slide.placeholders[1].text_frame.paragraphs:
             font1= x.font
             font1.name = 'Arial'
             font1.size = Pt(16)
@@ -30,8 +23,18 @@ def presentate(defined_list):
             font1.italic = False
         return slide
 
-    title_slide_layout = prs.slide_layouts[5]
 
+    def add_slide_img(prs, layout, img_path):
+        slide = prs.slides.add_slide(layout)
+        img_path =  ""+img_path
+        left =  Inches(2)
+        top = Inches(1)
+        width = Inches(5)
+        height = Inches(5)
+        pic = slide.shapes.add_picture(img_path, left, top, width, height)
+        
+    title_slide_layout = prs.slide_layouts[1]
+    title_slide_layimg=prs.slide_layouts[6]
     for i in range (0,len(defined_list)):     
         slide = add_slide(prs, title_slide_layout, defined_list[i]["Topic"],"\n".join(defined_list[i]["Summary"]))
 
